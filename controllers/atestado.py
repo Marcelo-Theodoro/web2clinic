2eef3fe92dcf557184b7aec580889807c7bbe13a
Marcelo-Theodoro <marcelotheodoro@outlook.com>
2015-11-17 14:37:47 -0200

    Adicionado grid ao atestado

--- controllers/atestado.py
+++ controllers/atestado.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def gerar_atestado():
-    id_paciente = request.args(0) or redirect(URL(c='paciente',
+    id_paciente = request.args(0) or redirect(URL(c='consulta',
                                                   f='todas_consultas'))
     paciente = db(db.pacientes.id == id_paciente).select().first()
     form = SQLFORM(db.atestados)
@@ -17,17 +17,37 @@ def gerar_atestado():
 
 
 def atestado():
-    id_atestado = request.args(0) or redirect(URL(c='paciente',
+    id_atestado = request.args(0) or redirect(URL(c='consulta',
                                                   f='todas_consultas'))
     atestado = db(db.atestados.id == id_atestado).select().first()
-    paciente = db(db.pacientes.id == atestado.id_paciente)
+    paciente = db(db.pacientes.id == atestado.id_paciente).select()
     return locals()
 
 
 def atestados():
-    # TODO: SQLFORM.grid -> paciente - dia do atestado
-    id_paciente = request.args(0) or redirect(URL(c='paciente',
-                                                  f='todas_consultas'))
-    paciente = db(db.pacientes.id == id_paciente).select().first()
-    atestados = db(db.atestados.id_paciente == id_paciente).select()
+    id_paciente = request.args(0)
+    if id_paciente:
+        paciente = db(db.pacientes.id == id_paciente).select().first()
+        if not paciente:
+            raise HTTP(404)
+    db.atestados.id_paciente.readable = True
+    haders = {'atestados.id_paciente': 'Paciente'}
+    links = [lambda row: A(SPAN('Visualizar',
+                                _class='icon magnifier icon-zoom-in\
+                                        glyphicon glyphicon-zoom-in'),
+                           _class='button btn btn-default',
+                           _href=URL(c='atestado', f='atestado',
+                                     args=[row.id]))]
+    grid = SQLFORM.grid(db.atestados.id_paciente == paciente.id
+                        if id_paciente else db.atestados,
+                        fields=[db.atestados.id_paciente,
+                                db.atestados.data_criacao],
+                        args=request.args[:1],
+                        csv=False,
+                        user_signature=False,
+                        headers=haders,
+                        details=False,
+                        links=links,
+                        editable=False,
+                        deletable=False)
     return locals()
