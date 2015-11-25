7bd76bfee7861fd804a6868dd8a0aef2f9248f50
Marcelo-Theodoro <marcelotheodoro@outlook.com>
2015-11-24 23:45:07 -0200

    Adicionada informações para default/index(): qtd_consultas_dia, qtd_consultas_mes, qtd_agendamentos_dia, qtd_agendamentos_mes

--- controllers/default.py
+++ controllers/default.py
@@ -1,9 +1,33 @@
 # -*- coding: utf-8 -*-
+# TODO: agendamentos por dia, agendamentos por mês, consultas hoje, consultas neste mês
 
 def index():
-    total_pacientes = db(db.pacientes).count()
-    total_agendamentos = db(db.agendamentos).count()
-    total_consultas = db(db.consultas).count()
+    from datetime import datetime
+    data = datetime.now().date()
+    mes = datetime.now().date().strftime(format='%m/%Y')
+    # Consultas
+    pacientes = db(db.pacientes).select()
+    agendamentos = db(db.agendamentos).select()
+    consultas = db(db.consultas).select()
+    # Total pacientes cadastrados
+    total_pacientes =  len(pacientes)
+    # Total de agendamentos
+    total_agendamentos = len(agendamentos)
+    # Total de consultas
+    total_consultas = len(consultas)
+    # Total agendamentos do dia
+    qtd_agendamentos_dia = len([i for i in agendamentos
+                                if i.dia == data])
+    # Total agendamentos mês
+    qtd_agendamentos_mes = len([i for i in agendamentos
+                                if i.dia.strftime(format='%m/%Y') == mes])
+    # Total consultas do dia
+    qtd_consultas_dia = len([i for i in consultas
+                                if i.dia == data])
+    # Total consultas mês
+    qtd_consultas_mes = len([i for i in consultas
+                                if i.dia.strftime(format='%m/%Y') == mes])
+
     itens = [i['form'] for i in tipos_consultas]
     lista = []
     for item in itens:
@@ -12,5 +36,4 @@ def index():
             label = [i['label'] for i in tipos_consultas
                      if item == i['form']][0]
             lista.append({'label': label, 'qtd': qtd_consultas})
-    # TODO: Consultas/agendamentos por dia, consultas/agendamentos por mês.
     return locals()
